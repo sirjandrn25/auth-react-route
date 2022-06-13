ad05b0c245a0097494249ca9c4d66277f16b1498
sirjandrn25 <56628702+sirjandrn25@users.noreply.github.com>
2022-06-13 17:21:55 +0545

    Update api.js
    just fun

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 const signUpUrl =
-  'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAKuP5sIym6HDWq84xKcf9FVNdhMvEWwek'
+  'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API-KEY]'
 
 const signInUrl =
-  'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAKuP5sIym6HDWq84xKcf9FVNdhMvEWwek'
+  'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API-KEY]'
 
 export const signUpRequest = async (data) => {
   const config = {
